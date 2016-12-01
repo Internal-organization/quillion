ffa94b10010cff1964b0fe1fc35f90d535585377
dbougearel <david.bougearel@gmail.com>
2016-12-01 09:13:52 +0100

    Update import.sql

--- main/src/main/resources/import.sql
+++ main/src/main/resources/import.sql
@@ -2,7 +2,7 @@ insert into Resource (id,name) values (1,'Fer');
 insert into Resource (id,name) values (2,'Acier');
 insert into Resource (id,name) values (3,'Titane');
 
-insert into User (id,first_name,last_name,age,email) values (1,'David','Bougearel',20,'david.bougearel@capgemini.com');
+insert into User (id,first_name,last_name,age,email) values (1,'David','Bougearel',20,'david.bougearel@gmail.com');
 
 insert into User_Resource (version,user_id,resource_id,quantity) values(1,1,1,5000);
 insert into User_Resource (version,user_id,resource_id,quantity) values(1,1,2,2000);
